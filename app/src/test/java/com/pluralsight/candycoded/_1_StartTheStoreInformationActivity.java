05486ab3cff140d4ebf3c36e68eec5572aa37ac2
Blannon <63664876+Blannon@users.noreply.github.com>
2022-08-19 07:12:19 +0300

    passed test

--- app/src/test/java/com/pluralsight/candycoded/_1_StartTheStoreInformationActivity.java
+++ app/src/test/java/com/pluralsight/candycoded/_1_StartTheStoreInformationActivity.java
@@ -22,18 +22,20 @@ import static org.junit.Assert.assertTrue;
 @PrepareForTest({AppCompatActivity.class, MainActivity.class, Intent.class, InfoActivity.class})
 @RunWith(PowerMockRunner.class)
 public class _1_StartTheStoreInformationActivity {
-    private static MainActivity activity;
+ public static void main(String[] args) {
 
+}
     private static boolean onOptionsItemSelected_result = true;
     private static boolean called_Intent = false;
     private static boolean called_Intent_correctly = false;
     private static boolean called_startActivity = false;
 
     // Mockito setup
+    @SuppressWarnings("ConstantConditions")
     @BeforeClass
-    public static void setup() throws Exception {
+    public static void setup() {
         // Spy on a MainActivity instance.
-        activity = PowerMockito.spy(new MainActivity());
+        MainActivity activity = PowerMockito.spy(new MainActivity());
         // Create a fake Bundle to pass in.
         Bundle bundle = Mockito.mock(Bundle.class);
         // Create a spy Intent to return from new Intent().
@@ -90,26 +92,26 @@ public class _1_StartTheStoreInformationActivity {
     }
 
     @Test
-    public void mainactivity_onoptionitemselected_return_super() throws Exception {
+    public void mainactivity_onoptionitemselected_return_super() {
         override_mainactivity_onoptionitemselected();
         assertFalse("onOptionsItemSelected() does not return call to super.", onOptionsItemSelected_result);
     }
 
     @Test
-    public void create_intent_infoactivity() throws Exception {
+    public void create_intent_infoactivity() {
         override_mainactivity_onoptionitemselected();
         assertTrue("The Intent was not created.", called_Intent);
         assertTrue("The Intent was created but with the wrong parameters. @intent-infoactivity", called_Intent_correctly);
     }
 
     @Test
-    public void startactivity_infoactivity() throws Exception {
+    public void startactivity_infoactivity() {
         override_mainactivity_onoptionitemselected();
         assertTrue("The method startActivity() was not called.", called_startActivity);
     }
 
     @Test
-    public void override_mainactivity_onoptionitemselected() throws Exception {
+    public void override_mainactivity_onoptionitemselected() {
         // Determine if the method OnOptionsItemSelected() is implemented in MainActivity
         // or just in the Base class
         Class<?> myClass = null;
